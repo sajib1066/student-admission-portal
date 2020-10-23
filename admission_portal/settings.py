f19ffe531a0a28cff5d2933cb3f4b22155f28946
Sajib Hossain <sajib1066@gmail.com>
2020-10-23 19:50:48 +0600

    setup bootstrap

--- admission_portal/settings.py
+++ admission_portal/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'admission_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ('static', )
